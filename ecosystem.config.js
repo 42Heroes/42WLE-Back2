79c775de35785b84f9a4de336edacbe54f0f2567
jooocy <hyean2302@gmail.com>
2022-10-27 14:46:39 +0900

    WLE - Change port 3000 to 8080

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'aws-codedeploy',
       script: 'npx',
-      args: 'serve -s build -l 3000 -n',
+      args: 'serve -s build -l 8080 -n',
       interpreter: 'none',
       env: {
         NODE_ENV: 'development',
